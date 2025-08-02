b0c55ffe1c5348997e5370bb719a275eb55bb32f
Dalis Keo <Dalis_keo@hotmail.com>
2025-08-02 19:33:21 +0200

    Fix routing and base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/AiCollections/', // your repo name
   plugins: [react()],
   test: {
     globals: true,
