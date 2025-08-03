d0cf21a07532baee3a516f29dd947dadab99de9a
Dk3o <Dk3o@users.noreply.github.com>
2025-08-03 02:32:11 +0200

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { ScrollToTopButton } from './Components/Buttons';
 function App() {
   return(
     <>
-      <Router>
+      <Router basename='/AiCcollections'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
